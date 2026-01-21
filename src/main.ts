aaa769acc2c43cdae949a0006eb3d2451113df9f
Seyi Obadeyi <obadeyi04@gmail.com>
2026-01-21 18:39:58 +0100

    pagination and caching

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
 
   // Enable CORS
   app.enableCors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000' 'https://ebonybrucetravel-a4uy.vercel.app',
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
     credentials: true,
   });
 
