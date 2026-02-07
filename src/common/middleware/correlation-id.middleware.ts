af16bb1b21bb496c3f075ed9f5c0584e2d081797
Seyi Obadeyi <obadeyi04@gmail.com>
2026-02-07 21:29:50 +0100

    uuid fix

--- src/common/middleware/correlation-id.middleware.ts
+++ src/common/middleware/correlation-id.middleware.ts
@@ -1,11 +1,11 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-import { v4 as uuidv4 } from 'uuid';
+import { randomUUID } from 'crypto';
 
 @Injectable()
 export class CorrelationIdMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    const correlationId = (req.headers['x-correlation-id'] as string) || uuidv4();
+    const correlationId = (req.headers['x-correlation-id'] as string) || randomUUID();
     (req as any).correlationId = correlationId;
     res.setHeader('X-Correlation-ID', correlationId);
     next();
