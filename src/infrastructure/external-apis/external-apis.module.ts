a5c353d8ca070642c4d51744e835a06e6913acaa
Obafemi James <129944838+Cortouchmedia@users.noreply.github.com>
2026-06-08 16:07:18 +0100

    Update

--- src/infrastructure/external-apis/external-apis.module.ts
+++ src/infrastructure/external-apis/external-apis.module.ts
@@ -3,14 +3,13 @@ import { DuffelService } from './duffel/duffel.service';
 import { TripsAfricaService } from './trips-africa/trips-africa.service';
 import { BookingComService } from './booking-com/booking-com.service';
 import { AmadeusService } from './amadeus/amadeus.service';
-import { AmadeusHotelController } from './amadeus/amadeus.controller';
 import { WakanowService } from './wakanow/wakanow.service';
 import { GooglePlacesModule } from './google-places/google-places.module';
 import { HotelbedsModule } from './hotelbeds/hotelbeds.module';
 
 @Module({
   imports: [GooglePlacesModule, HotelbedsModule],
-  controllers: [AmadeusHotelController],
+  controllers: [],
   providers: [
     DuffelService, 
     TripsAfricaService, 
