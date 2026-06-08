e94b23a5072b4be580fe6f0721af398c7b6244b4
Obafemi James <129944838+Cortouchmedia@users.noreply.github.com>
2026-06-08 15:20:26 +0100

    module update

--- src/infrastructure/external-apis/external-apis.module.ts
+++ src/infrastructure/external-apis/external-apis.module.ts
@@ -3,12 +3,29 @@ import { DuffelService } from './duffel/duffel.service';
 import { TripsAfricaService } from './trips-africa/trips-africa.service';
 import { BookingComService } from './booking-com/booking-com.service';
 import { AmadeusService } from './amadeus/amadeus.service';
+import { AmadeusHotelController } from './amadeus/amadeus.controller';
 import { WakanowService } from './wakanow/wakanow.service';
 import { GooglePlacesModule } from './google-places/google-places.module';
 import { HotelbedsModule } from './hotelbeds/hotelbeds.module';
+
 @Module({
   imports: [GooglePlacesModule, HotelbedsModule],
-  providers: [DuffelService, TripsAfricaService, BookingComService, AmadeusService, WakanowService],
-  exports: [DuffelService, TripsAfricaService, BookingComService, AmadeusService, WakanowService, GooglePlacesModule, HotelbedsModule],
+  controllers: [AmadeusHotelController],
+  providers: [
+    DuffelService, 
+    TripsAfricaService, 
+    BookingComService, 
+    AmadeusService, 
+    WakanowService
+  ],
+  exports: [
+    DuffelService, 
+    TripsAfricaService, 
+    BookingComService, 
+    AmadeusService, 
+    WakanowService, 
+    GooglePlacesModule, 
+    HotelbedsModule
+  ],
 })
 export class ExternalApisModule { }
